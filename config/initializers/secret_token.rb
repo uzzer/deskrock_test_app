bbadd585cc15dd4718bf4603fab75dc936bfd798
Mihhail Sidorin <msidorin@gmail.com>
2013-10-10 19:55:47 +0300

    Initial template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeskrockTestApp::Application.config.secret_key_base = '53ccf271076a2527b62dd5a04d9dedd6db914473f51ded1e190c58f1062d1ec0cee4951c430439b10e68c86f25790866accc54f16a825e7d31c8cc55963af2f5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DeskrockTestApp::Application.config.session_store :cookie_store, key: '_deskrock_test_app_session'
