41a7b116194b967a1f26b08d5a044616fe6da7ad
Mihhail Sidorin <msidorin@gmail.com>
2013-10-10 20:49:34 +0300

    Add Twitter bootstrap 3 support and demonstrate it on about page

--- Gemfile
+++ Gemfile
@@ -9,6 +9,9 @@ gem 'sqlite3'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
 
+# Use Bootstrap gem for Bootstrap framework
+gem 'bootstrap-sass-rails'
+
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 DeskrockTestApp::Application.routes.draw do
+  get "pages/about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'pages#about'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
