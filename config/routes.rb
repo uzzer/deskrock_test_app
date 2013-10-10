5346571ab362d1399582486a5ea06be55f9ed637
Mihhail Sidorin <msidorin@gmail.com>
2013-10-10 22:34:39 +0300

    Redirect application to stocks index as front page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 DeskrockTestApp::Application.routes.draw do
+  resources :stocks
+
   get "pages/about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#about'
+  root 'stocks#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
