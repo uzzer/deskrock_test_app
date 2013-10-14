342ab9aad890d0a8ebf40d5bba6c57e1eb384a11
Mihhail Sidorin <msidorin@gmail.com>
2013-10-14 15:09:29 +0300

    Fix buttons view to general UI style

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,11 @@ module DeskrockTestApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance|
+      "<div class=\"field_with_errors control-group has-error\">#{html_tag}</div>".html_safe
+    }
   end
 end
+
+
